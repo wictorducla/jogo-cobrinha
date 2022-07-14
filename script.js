8efca5270a7d8ea3ef3490297cf8303a66b7812d
Wictor Ducla <wictor.ducla14@gmail.com>
2022-07-14 13:13:01 -0300

    lendo teclado e impedindo que cobrinha suma na parede

--- script.js
+++ script.js
@@ -20,16 +20,34 @@ function criarCobriniha(){
     }
 }
 
+document.addEventListener('keydown',update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobriniha();
+
     let snakeX = snake[0].x;
     let snakeY =  snake[0].y;
+
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
+
     snake.pop();
+
     let newHead  =  {
         x: snakeX,
         y: snakeY
